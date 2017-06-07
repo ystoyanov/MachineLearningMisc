fd8beb6298c9ebf977fa611a8ff50ce962d95155
Gopi Subramanian <gsubramanian@tycoint.com>
2017-06-07 14:04:28 -0400

    Quantile Regression code added

---
+++ QuantileRegression.py
@@ -0,0 +1,159 @@
+"""
+ Code for blog article on Quantile Regression
+
+"""
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+## Generate some data with constant variance /noise
+x = np.arange(100).reshape(100,1)
+intercept_ = 6
+slope_ = 0.1
+## non constant error
+error_ = np.random.normal(size = (100,1), loc = 0.0, scale = 1)
+## Regression equation
+y = intercept_ + slope_ * x + error_
+
+plt.figure(1)
+plt.scatter(x, y)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Data with constant variance")
+
+## Let us do a least square regression on the above dataset
+from sklearn.linear_model import LinearRegression
+
+model1 = LinearRegression(fit_intercept = True, normalize = False)
+model1.fit(x, y)
+
+y_pred1 = model1.predict(x)
+
+print("Mean squared error: {0:.2f}"
+      .format(np.mean((y_pred1 - y) ** 2)))
+print('Variance score: {0:.2f}'.format(model1.score(x, y)))
+
+## Plot the regression
+plt.figure(2)
+plt.scatter(x, y,  color='black')
+plt.plot(x, y_pred1, color='blue',
+         linewidth=3)
+
+plt.xticks(())
+plt.yticks(())
+plt.xlabel("x")
+plt.ylabel("y and predicted y")
+plt.title("Linear regression")
+
+
+
+## Generate some data with non-constant variance
+x_ = np.arange(100).reshape(100,1)
+intercept_ = 6
+slope_ = 0.1
+## Non constant variance
+var_ = 0.1 + 0.05 * x_
+## non constant error
+error_ = np.random.normal(size = (100,1), loc = 0.0, scale = var_)
+## Regression equation
+y_ = intercept_ + slope_ * x + error_
+
+plt.figure(3)
+plt.scatter(x_, y_)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Data with non-constant variance")
+
+
+## Try to fit a linear regression
+model2 = LinearRegression(fit_intercept = True, normalize = False)
+model2.fit(x_, y_)
+
+y_pred2 = model2.predict(x_)
+
+print
+print("Mean squared error: {0:.2f}"
+      .format(np.mean((y_pred2 - y_) ** 2)))
+print('Variance score: {0:.2f}'.format(model1.score(x_, y_)))
+
+## Plot the regression
+plt.figure(4)
+plt.scatter(x_, y_,  color='black')
+plt.plot(x_, y_pred2, color='blue',
+         linewidth=3)
+
+plt.xticks(())
+plt.yticks(())
+plt.xlabel("x")
+plt.ylabel("y and predicted y")
+plt.title("Linear regression on data with non-constant variance")
+
+
+
+## Quantile regression for the median, 0.5th quantile
+import pandas as pd 
+data = pd.DataFrame(data = np.hstack([x_, y_]), columns = ["x", "y"])
+print data.head()
+
+import statsmodels.formula.api as smf
+
+
+mod = smf.quantreg('y ~ x', data)
+res = mod.fit(q=.5)
+print(res.summary())
+
+
+## Build the model for other quantiles
+quantiles = np.arange(0.1,1,0.1)
+print quantiles 
+models = []
+params = []
+
+for qt in quantiles:
+	print qt
+	res = mod.fit(q = qt )
+	models.append(res)
+	params.append([qt, res.params['Intercept'], res.params['x']] + res.conf_int().ix['x'].tolist())
+
+
+params = pd.DataFrame(data = params, columns = ['qt','intercept','x_coef','cf_lower_bound','cf_upper_bound'])
+
+print params
+
+## Let us plot the 10th, 50th and 90th percentile
+plt.figure(5)
+plt.scatter(x_, y_,  color='black')
+plt.plot(x_, y_pred2, color='blue',
+         linewidth=3, label='Lin Reg')
+
+y_pred3 = models[0].params['Intercept'] + models[0].params['x'] * x_
+plt.plot(x_, y_pred3, color='red',
+         linewidth=3, label='Q Reg : 0.1')
+
+y_pred4 = models[4].params['Intercept'] + models[4].params['x'] * x_
+plt.plot(x_, y_pred4, color='green',
+         linewidth=3, label='Q Reg : 0.5')
+
+
+y_pred5 = models[8].params['Intercept'] + models[8].params['x'] * x_
+plt.plot(x_, y_pred5, color='cyan',
+         linewidth=3, label='Q Reg : 0.9')
+
+
+plt.xticks(())
+plt.yticks(())
+plt.xlabel("x")
+plt.ylabel("y and predicted y")
+plt.title("Quantile regression on data with non-constant variance")
+plt.legend()
+
+
+## Plot the changes in the quantile coeffiecients
+plt.figure(6)
+params.plot(x = 'qt', y = ['x_coef','cf_lower_bound', 'cf_upper_bound'], 
+	title = 'Slope for different quantiles', kind ='line', style = ['b-','r--','g--'])
+
+
+
+plt.show()
+
